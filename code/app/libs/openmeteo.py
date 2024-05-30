db27208cd557a9012b67ef2dcc809b6910640f8d
kBasahr <khyrul.bashar@gigalogy.com>
2024-05-30 13:59:51 +0600

    coolest district list with dynamic hour and result size

--- code/app/libs/openmeteo.py
+++ code/app/libs/openmeteo.py
@@ -19,6 +19,8 @@ openmeteo = openmeteo_requests.Client(session=retry_session)
 # The order of variables in hourly or daily is important to assign them correctly below
 openmeteo_url = "https://api.open-meteo.com/v1/forecast"
 
+latest_weather_data = None
+coolest_weather_data = {}
 
 def fetch_districts():
     try:
@@ -35,7 +37,8 @@ def fetch_weather_data(latitudes, longitudes):
     payload = {
         "latitude": latitudes,
         "longitude": longitudes,
-        "hourly": "temperature_2m"
+        "hourly": "temperature_2m",
+        "forecast_days": 7
     }
     try:
         response = requests.get(openmeteo_url, params=payload)
@@ -43,15 +46,27 @@ def fetch_weather_data(latitudes, longitudes):
             data = response.json()
             return data
     except Exception as e:
-        logging.error("Weather data fetching from openmeteo failed.")
+        logging.error(str(e))
         return None
 
 
 def save_weather_data(data):
+    global latest_weather_data
     filepath = path.abspath(path.join(basepath, "..", "bd-districts-weather.json"))
     try:
         with open(filepath, "w+") as weather_file:
             json.dump(data, weather_file)
+            latest_weather_data = data
+    except Exception:
+        logging.error("Failed to save the weather data to disk.")
+
+
+def load_weather_data():
+    global latest_weather_data
+    filepath = path.abspath(path.join(basepath, "..", "bd-districts-weather.json"))
+    try:
+        with open(filepath, "r+") as weather_file:
+            latest_weather_data = json.load(weather_file)
     except Exception:
         logging.error("Failed to save the weather data to disk.")
 
@@ -73,18 +88,32 @@ def collect_weather_data_for_next_7_days():
     weather_data = fetch_weather_data(all_latitudes, all_longitudes)
 
     if weather_data is None:
-        logging.info("weather data fetching failed")
+        logging.error("Weather data fetching from openmeteo failed.")
         return None
 
-    district_map = dict()
     for district, dist_weather_data in zip(districts, weather_data):
-        district["weather"] = dist_weather_data
-
-        # we store the district data mapped to their id, this will sped up the process later
-        district_map[district["id"]] = district
-
-    save_weather_data(district_map)
+        hourly_temperatures = dist_weather_data["hourly"]["temperature_2m"]
+        average_temperatures = dict()
+        for hour in range(24):
+            hour_temperatures = [temp for temp in hourly_temperatures[hour::24]]
+            average_temp = sum(hour_temperatures)/len(hour_temperatures)
+            average_temperatures[hour] = round(average_temp, 2)
+        district["average_temperatures"] = average_temperatures
+    save_weather_data(districts)
 
     return {
         "collected": True
     }
+
+
+def get_coolest_districts(hour=14, size=10):
+    if latest_weather_data is None:
+        load_weather_data()
+    sorted_districts = sorted(latest_weather_data, key=lambda x: x["average_temperatures"][hour])
+    coolest_districts = sorted_districts[:size]
+    new_list = []
+    for obj in coolest_districts:
+        o = {key: value for key, value in obj.items() if key != "average_temperatures"}
+        o["average_temperature"] = obj["average_temperatures"][hour]
+        new_list.append(o)
+    return new_list
--- code/app/main.py
+++ code/app/main.py
@@ -1,12 +1,21 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
-from app.libs.openmeteo import collect_weather_data_for_next_7_days
+from app.libs.openmeteo import (
+    collect_weather_data_for_next_7_days,
+    get_coolest_districts
+)
+
+collect_weather_data_for_next_7_days()
 
 app = FastAPI()
 
 
 @app.get("/coolest")
-def get_coolest_districts():
-    return collect_weather_data_for_next_7_days()
+def get(
+    hour: int = Query(default=14, le=23, ge=0),
+    size: int = Query(default=10, le=64, ge=0)
+):
+    return get_coolest_districts(hour, size)
+
